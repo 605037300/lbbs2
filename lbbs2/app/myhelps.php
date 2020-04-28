bf63dd238a3a2a6e94df5d6020b230a00aad6c4c
baojunchen <605037300@qq.com>
2020-04-28 16:00:19 +0800

    话题分类

--- lbbs2/app/myhelps.php
+++ lbbs2/app/myhelps.php
@@ -4,3 +4,8 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+
+function nav_active($categoryid){
+    return active_class(if_route('categories.show')&&if_route_param('category',$categoryid));
+}
