898a96fc4429b3ea10ad3ea190589db871901b2f
baojunchen <605037300@qq.com>
2020-05-01 12:15:05 +0800

    文章上传图片

--- lbbs2/app/Http/Controllers/TopicsController.php
+++ lbbs2/app/Http/Controllers/TopicsController.php
@@ -49,25 +49,27 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功');
 	}
 
-	public function uploadImage(Request $request,UploadImageHandler $upimghandler){
-		$data = [
+	public function uploadImage(Request $request, uploadImageHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
             'success'   => false,
             'msg'       => '上传失败!',
-            'file_path' => '123'
-		];
-		
-		if($file=$request->upload_file){
-			$result=$upimghandler->save($file,'topics',\Auth::id(),1024);
-			if($result){
-				$data['success']=true;
-				$data['msg']="上传成功";
-				$data['file_path']=$result['path'];
-			}
-		}
-		dd($data);
-		return $data;
-	}
-
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
+    }
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
